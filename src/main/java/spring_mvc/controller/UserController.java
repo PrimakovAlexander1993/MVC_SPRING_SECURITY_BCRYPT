512c2b1a31bd07cf4667bb3755eb8ecda9cd309a
Alexander Primakov <primakovalexander07041993@gmail.com>
2020-08-21 08:07:57 +0300

    final update

--- src/main/java/spring_mvc/controller/UserController.java
+++ src/main/java/spring_mvc/controller/UserController.java
@@ -1,6 +1,7 @@
 package spring_mvc.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/spring_mvc/security/SecurityConfig.java
+++ src/main/java/spring_mvc/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder()  {
         return new BCryptPasswordEncoder();  //Кодировка пароля
     }
 }
